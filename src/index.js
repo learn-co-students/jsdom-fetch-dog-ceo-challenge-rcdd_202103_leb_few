0fdc5dad6c12b10823ee3207e377ade16a1e6d70
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-24 18:36:53 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,92 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+//fetch images
+function fetImages(){
+  return fetch(imgUrl)
+  .then(response=>response.json())
+/////  .then(json ==>console.log(json))
+}
+
+//Get Img
+function addImages (imageSRC){
+  let imagecontainer = document.getElementById('dog-image-container')
+  let img = document.createElement('img')
+      img.src = imageSRC
+
+imagecontainer.appendChild(img)
+}
+//Add img
+function addimages(){
+  fetchImages()
+  .then(object => object.message.forEach (object => {addImages(object)}))
+}
+
+//fetch breeds
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+function fetchBreeds(){
+  return fetch(breedUrl)
+  .then(response =>response.json())
+}
+
+//Add Breed list
+function AddBreedList(breedSRC){
+  let breedList = document.getElementById(('dog-breeds'))
+  let breedLi = document.createElement('li')
+  breedLi.innerText= breedSRC
+  breedList.appendChild(breedLi)
+}
+
+//Add breedLi
+function addBreeds(){
+  fetchBreeds()
+  .then(breeds =>object.keys(breeds.message).forEach(breed => {addList(breed)}))
+}
+
+//Change fontColor of li
+function changeTextcolor(){
+  document.addEventListener('click',event=> {
+    if (event.target.tagName ==='li'){
+      event.target.style.color = 'pink';
+    }
+  })
+}
+
+//push breed into array
+const breedarray =[]
+function  pushBreed(){
+  fetch (breedUrl)
+  .then (response => response.json()
+  .then (breeds => object.keys(breeds.message).forEach(dog => {breedArray.push(dog)})))
+}
+
+//sleect first letter of breed
+function filterDogs(letter){
+  return breedArray.filter(dog=>dog[0] === letter)
+}
+
+//dropdown
+const dogBreeds = document.getElementById('dog-breeds')
+const dropDown = document.getElementById('breed-dropdown')
+
+function dropDownMenu(){
+dropDown.addEventListener('change',function(event){
+dogBreeds.innetHtml =" "
+let filterD = filterDogs(dropDown.value)
+.map(dog => `<li>${dog}</li>`).join('')
+dogBreeds.innerHTML += filterD
+})
+}
+
+//document onload !!
+document.addEventListener('DOMContentLoaded', function(){
+    fetchImages()
+    addImages()
+    postImages()
+    fetchBreeds()
+    addList()
+    postBreeds()
+    fetchBreedsList()
+    findByDropdownMenu()
+    changeTextColor()
+})
