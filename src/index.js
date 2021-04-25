2dd2045f1e96d1ca4f916d3af69520356f2b5011
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 17:10:16 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+//
+// console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+const div = document.querySelector("#dog-image-container")
+// // document.addEventListener("DOMContentLoaded", function a () {
+    fetch(imgUrl)
+    .then((response) => response.json())
+       .then((json) => {
+         console.log(json);
+       a.message.forEach(function(messag)){
+
+
+          div.innerHTML+=
+            `<img src="${iimgUrl}">`
+            )
+          }  }
+;
