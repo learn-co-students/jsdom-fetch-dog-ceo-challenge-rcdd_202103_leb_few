5b70f7a36d3c0befe55fde660c69a8fd5ae7a2d6
Israe Moubayed <israetarek@yahoo.com>
2021-04-27 19:28:08 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+
+})
